333b9e5e4feb573d47c93bf3d1f8eb706b4dbaf2
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-08 15:52:07 -0400

    adicionando produtos oa carrinho com informacoes

--- js/app.js
+++ js/app.js
@@ -4,17 +4,20 @@ let produto = document.getElementById('produto').value;
 let nomeProduto = produto.split('-')[0];
 let valorUnitario = produto.split('R$')[1];
 let quantidade = document.getElementById('quantidade').value;
-let preco = quantidade * valorUnitario;
-
-alert(nomeProduto);
-alert(valorUnitario);
-alert(quantidade);
-alert(preco);
-
-
 // calcular o preço, o nosso subtotal
+let preco = quantidade * valorUnitario;
+let carrinho = document.getElementById('lista-produtos');
+
+carrinho.innerHTML = `<section class="carrinho__produtos__produto">
+<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+</section>`
 // adicionar no carrinho
 // atualizar o valor total da compra
+
+let valorTotal = document.getElementById('valor-total');
+
+valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${preco}</span>`
+
 }
 
 function limpar() {
