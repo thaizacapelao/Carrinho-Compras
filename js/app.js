e618f6d4db498dd407dcafdb9cd999b3064218a5
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-08 17:02:30 -0400

    Calculando valor total do carrinho e funcao de limpar

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
+let totalGeral = 0;
+limpar();
+
 function adicionar() {
-// recuperar valores, nome do produto, quantidade e valor
-let produto = document.getElementById('produto').value;
-let nomeProduto = produto.split('-')[0];
-let valorUnitario = produto.split('R$')[1];
-let quantidade = document.getElementById('quantidade').value;
-// calcular o preço, o nosso subtotal
-let preco = quantidade * valorUnitario;
-let carrinho = document.getElementById('lista-produtos');
-// adicionar no carrinho
-carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+  // recuperar valores, nome do produto, quantidade e valor
+  let produto = document.getElementById("produto").value;
+  let nomeProduto = produto.split("-")[0];
+  let valorUnitario = produto.split("R$")[1];
+  let quantidade = document.getElementById("quantidade").value;
+  // calcular o preço, o nosso subtotal
+  let preco = quantidade * valorUnitario;
+  let carrinho = document.getElementById("lista-produtos");
+  // adicionar no carrinho
+  carrinho.innerHTML =
+    carrinho.innerHTML +
+    `<section class="carrinho__produtos__produto">
 <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
-</section>`
-// atualizar o valor total da compra
-let valorTotal = document.getElementById('valor-total');
+</section>`;
+  // atualizar o valor total da compra
 
-valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${preco}</span>`
+  totalGeral = totalGeral + preco;
+  let valorTotal = document.getElementById("valor-total");
 
+  valorTotal.innerHTML = `<span class="texto-azul" id="valor-total">R$${totalGeral}</span>`;
+  document.getElementById("quantidade").value = 0;
 }
 
 function limpar() {
-    
+  totalGeral = 0;
+  document.getElementById("lista-produtos").innerHTML = "";
+  document.getElementById("valor-total").textContent = "R$ 0";
 }
